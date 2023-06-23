dce3ee0609e8960263c12187a9c6328a572bd297
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-06-23 07:58:39 +0100

    insertHeap

---
+++ Data structure/Binary heap.js
@@ -0,0 +1,28 @@
+class MaxBinaryHeap {
+    constructor() {
+        this.val = []
+    }
+
+    insert(element) {
+        this.val.push(element)
+        this.bubbleUp()
+    }
+
+    bubbleUp() {
+        let index = this.val.length - 1
+        const element = this.val[index]
+        while (index > 0) {
+            let parentIdx = Math.floor((index - 1) / 2)
+            let parent = this.val[parentIdx]
+            if (element <= parent) break
+            if(element > parent){ 
+                this.val[parentIdx] = element
+                this.val[index] = parent
+            }
+            index = parentIdx
+        }
+    }
+}
+
+// [41,39,33,18,27,12,55]
+//  0  1  2  3  4  5  6
