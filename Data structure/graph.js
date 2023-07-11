eaf378c912634568b4486455ef316699c7c141da
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-07-11 12:57:16 +0100

    graph traversal dfs

--- Data structure/graph.js
+++ Data structure/graph.js
@@ -20,7 +20,7 @@ class Graph {
     removeEdge(v1, v2) {
         if (this.adjacencyList.hasOwnProperty(v1) && this.adjacencyList.hasOwnProperty(v2)) {
             this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
-            this.adjacencyList[v2] = this.adjacencyList[v1].filter(v => v !== v1)
+            this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
         }
     }
 
@@ -31,9 +31,40 @@ class Graph {
         }
         delete this.adjacencyList[vertex]
     }
+
+    dfs(start) {
+        const result = []
+        let visited = {}
+        const adjacencyList = this.adjacencyList
+            
+        function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true
+            result.push(vertex)
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    return dfs(neighbor)
+                }
+            })
+        }
+        dfs(start);
+        
+        return result
+    }
 }
 
-let g = new Graph()
-g.addVertex("Kwara")
-g.addVertex("Osun")
-g.addVertex("Lagos")
+let g = new Graph() 
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
